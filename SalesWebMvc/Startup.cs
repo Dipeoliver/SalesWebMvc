09ff51722531d408db393067498c6e09cc8e8a11
Diego Oliveira <dipeoliver@outlook.com>
2021-09-30 16:46:54 -0300

    Preparing sales search navigation views

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -45,6 +45,7 @@ namespace SalesWebMvc
             services.AddScoped<SellerServices>();
             services.AddScoped<DepartmentService>();
 
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
